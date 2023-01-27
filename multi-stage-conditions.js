8ac13b7c00fc3d45da6ce73409f532e6872c8bd9
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-27 21:57:28 +0600

    multi-stage-conditions file add

---
+++ multi-stage-conditions.js
@@ -0,0 +1,17 @@
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if(danishPrice < money){
+    console.log('ami danis khamu');
+} 
+else if(butterBread < money){
+    console.log('ami butter khamu');
+}
+else if(toastBiscuit < money){
+    console.log('toastBiscuit khamu ami 1 tta');
+}
+else{
+    console.log('tea khabo batas diya');
+}
