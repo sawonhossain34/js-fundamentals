770be556b9e30ebf6457e012cf9151fbdf3ccd5a
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-27 21:29:22 +0600

    multiple-conditions file add

---
+++ multiple-conditions.js
@@ -0,0 +1,10 @@
+var isGraduated = true;
+var salary = 80000;
+var car = 0;
+
+if(isGraduated = true &&(salary > 75000 || car >= 1)){
+    console.log('aco bia kore fali');
+}
+else{
+    console.log('tomar friend er phone number daw');
+}
