9df80a1ab11944cfcf3b485c00805051cb062c22
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-27 21:15:54 +0600

    conditional file add

---
+++ conditional.js
@@ -0,0 +1,10 @@
+var iphonePrice = 79000;
+var myBudget = 9500;
+// if iphone price less than my myBudget. I will buy the iphone.
+
+if(iphonePrice < myBudget){
+    console.log('I will buy the iphone');
+}
+else{
+    console.log('kinbo na');
+}
