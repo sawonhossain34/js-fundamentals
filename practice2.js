368ca9bc090bfb5cfa33c39d787b16e113418282
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-28 15:06:07 +0600

    practice2 file add gread point

---
+++ practice2.js
@@ -0,0 +1,37 @@
+// var sawon = 85;
+// var tom = 66;
+// var jane = 95;
+// var peter = 56;
+var marks = 101;
+
+if(marks >= 80 && marks <= 100 ){
+    console.log('A+');
+}else if(marks >= 70 && marks <= 79){
+    console.log('A');
+}else if(marks >= 60 && marks <= 69){
+    console.log('A-');
+}else if(marks >= 50 && marks <= 59){
+    console.log('B');
+}else if(marks >= 40 && marks <= 49){
+    console.log('C');
+}else if(marks >= 33 && marks <= 39){
+    console.log('D');
+}else if(marks >= 0 && marks <= 32){
+    console.log('Fail');
+}else if(marks < 0 && marks > 100){
+    console.log('No Result');
+}
+
+
+
+
+// else if(marks >= 40 && marks <= 49){
+//     console.log('');
+// }else if(marks >= 50 && marks <= 49){
+//     console.log('C');
+// }else if(marks >= 60 && marks <= 59){
+//     console.log('B');
+// }else if(marks >= 80 && marks <= 59){
+//     console.log('A');
+// }
+
